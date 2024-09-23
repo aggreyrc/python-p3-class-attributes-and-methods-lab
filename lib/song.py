4517815cc2a429f636d3e712e0c41d9d8ada90e8
aggreyrc <aggreyronoh@gmail.com>
2024-09-23 23:00:04 +0300

    class attributes and methods

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,29 @@
+        
 class Song:
-    pass
+    count = 0
+    genres = set()
+    artists = set()
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        
+        # Increment the total song count
+        Song.count += 1
+        
+        # Track the genre
+        Song.genres.add(genre)
+        if genre in Song.genre_count:
+            Song.genre_count[genre] += 1
+        else:
+            Song.genre_count[genre] = 1
+        
+        # Track the artist
+        Song.artists.add(artist)
+        if artist in Song.artist_count:
+            Song.artist_count[artist] += 1
+        else:
+            Song.artist_count[artist] = 1
